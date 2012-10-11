c65bc27ec3949b7c7ddbb3239ea390b7d401cd15
Pasha <Pasha@MICROSOFT-PC>
2012-10-11 23:47:46 +0300

    startup

---
+++ docs/dump.sql
@@ -0,0 +1,70 @@
+/*
+SQLyog Ultimate v8.71 
+MySQL - 5.1.48-community : Database - stas
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`stas` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `stas`;
+
+/*Table structure for table `firms` */
+
+DROP TABLE IF EXISTS `firms`;
+
+CREATE TABLE `firms` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `firms` */
+
+insert  into `firms`(`id`,`name`,`description`) values (1,'Viko','Viko');
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `image_extension` varchar(8) DEFAULT NULL,
+  `price` varchar(8) NOT NULL,
+  `firm_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_products_firm_id_firms_id` (`firm_id`),
+  CONSTRAINT `FK_products_firm_id_firms_id` FOREIGN KEY (`firm_id`) REFERENCES `firms` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `products` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`login`,`password`) values (1,'admin','admin');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
